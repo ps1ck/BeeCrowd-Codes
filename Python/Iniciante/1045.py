1111cc69eae2ffa86f61853e858b3c4d0f265b78
Rian Mendonça do Vale <rianmvale@hotmail.com>
2025-01-07 21:39:29 -0300

    1045, aprendi a usar sorted

---
+++ Python/Iniciante/1045.py
@@ -0,0 +1,20 @@
+a, b, c = map(float, input().split())
+
+lados = sorted([a, b, c], reverse=True)
+A, B, C = lados
+
+if A >= (B + C):
+    print('NAO FORMA TRIANGULO')
+else:
+    if A**2 == (B**2 + C**2):
+        print('TRIANGULO RETANGULO')
+    elif A**2 > (B**2 + C**2):
+        print('TRIANGULO OBTUSANGULO')
+    elif A**2 < (B**2 + C**2):
+        print('TRIANGULO ACUTANGULO')
+    
+
+if A == B == C:
+    print('TRIANGULO EQUILATERO')
+elif A == B or B == C or C == A:
+    print('TRIANGULO ISOSCELES')
