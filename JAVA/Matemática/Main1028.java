0b29687484e62d9bd442350f32ddd651dceca0f7
Rian Mendonça do Vale <111614161+ps1ck@users.noreply.github.com>
2025-04-01 18:47:09 -0300

    JAVA matematica 1028

--- JAVA/Matemática/Main1028.java
+++ JAVA/Matemática/Main1028.java
@@ -1,11 +1,18 @@
 import java.io.IOException;
- 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class Main1028 {
- 
     public static void main(String[] args) throws IOException {
- 
         
- 
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+
+        for(int xi = 0; xi < i; xi++){
+            int x = sc.nextInt();int y = sc.nextInt();
+            int pilha = BigInteger.valueOf(x).gcd(BigInteger.valueOf(y)).intValue();
+            System.out.println(pilha);
     }
- 
+        sc.close();
+    }
 }
